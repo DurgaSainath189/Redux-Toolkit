2cf35b44b6b207c3c7e14b338d1ef883160555d7
Durga Sainath Dora <durgasainathdora@gmail.com>
2024-02-17 02:03:42 +0530

    provided the redux store to react

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { store } from "./app/store.js";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-)
+  </Provider>
+);
